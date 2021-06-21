0ba7f56d591f20acb227fbd2045aa840c6bf4d62
ReySebastien <rey.babou35@free.fr>
2021-06-21 02:38:23 +0200

    update menu

--- Controles.js
+++ Controles.js
@@ -4,7 +4,13 @@ class Controles extends Phaser.Scene{
     }
 create(){
     
-    
+        let retourButton = this.add.image(this.game.renderer.width / 2 - 330 , this.game.renderer.height / 3 - 150 , 'retourButton').setDepth(1);
+
+        retourButton.setInteractive();
+
+        retourButton.on("pointerdown", () => {
+            this.scene.start('Menu', {x : 960, y : 540});
+        })
     } // FIN CREATE ----------------------------------------------------------------
 
 } // FIN SCENE
